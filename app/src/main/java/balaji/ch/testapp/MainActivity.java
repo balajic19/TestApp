bc5bb86803f0633347243501bdaa20cad8b0fb0a
Balaji Chandupatla <balajichandupatla1@gmail.com>
2021-10-20 23:17:30 -0400

    Initial commit

---
+++ app/src/main/java/balaji/ch/testapp/MainActivity.java
@@ -0,0 +1,16 @@
+package balaji.ch.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
